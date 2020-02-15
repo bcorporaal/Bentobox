a86b5d5ecd1dc35462f31aa52a47bd4755ce9b4d
Bob <bob@reefscape.net>
2020-02-15 17:29:26 -0600

    refresh favicon design

--- site/templates/default.php
+++ site/templates/default.php
@@ -4,19 +4,16 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?= $site->title()->html() ?></title>
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="assets/favicons/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/favicons/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/favicons/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/favicons/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="assets/favicons/apple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="assets/favicons/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="assets/favicons/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="assets/favicons/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="assets/favicons/favicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="assets/favicons/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="assets/favicons/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="assets/favicons/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="assets/favicons/favicon-128.png" sizes="128x128" />
+    <link rel="icon" href="assets/favicons/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicons/favicon-16x16.png">
+    <link rel="manifest" href="assets/favicons/site.webmanifest">
+    <link rel="mask-icon" href="assets/favicons/safari-pinned-tab.svg" color="#FAB005">
+    <link rel="shortcut icon" href="favicon.ico">
+    <meta name="msapplication-TileColor" content="#333">
+    <meta name="msapplication-config" content="assets/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#333">
     <style>
       <?php snippet('fontselection'); ?>
       <?php include 'assets/css/bentobox.css'; ?>
@@ -30,7 +27,7 @@
         <div class="top-bar">
           <a href="<?= $site->panelUrl(); ?>">
             <div id="logo-container">
-              <?php include 'assets/images/bentobox_logo-'.$site->logo().'.svg'; ?>
+              <?php include 'assets/images/logo-'.$site->logo().'.svg'; ?>
             </div>
           </a>
         </div>
