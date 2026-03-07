525d70e4850aab0ca64673271520269717f5963f
Bob Corporaal <bob@reefscape.net>
2026-03-06 22:43:31 -0400

    switch to fonts.bunny.net

--- site/snippets/customfont.php
+++ site/snippets/customfont.php
@@ -2,92 +2,92 @@
 	switch ($fontfamily) {
 
 		case "inter":
-			$importurl = "https://fonts.googleapis.com/css?family=Inter";
+			$importurl = "https://fonts.bunny.net/css?family=Inter";
 			$fontfamilystring = "'Inter', sans-serif";
 			break;
 
 		case "fira_mono":
-			$importurl = "https://fonts.googleapis.com/css?family=Fira+Mono";
+			$importurl = "https://fonts.bunny.net/css?family=Fira+Mono";
 			$fontfamilystring = "'Fira Mono', monospace";
 			break;
 
 		case "fira_sans":
-			$importurl = "https://fonts.googleapis.com/css?family=Fira+Sans";
+			$importurl = "https://fonts.bunny.net/css?family=Fira+Sans";
 			$fontfamilystring = "'Fira Sans', sans-serif";
 			break;
 
 		case "offside":
-			$importurl = "https://fonts.googleapis.com/css?family=Offside";
+			$importurl = "https://fonts.bunny.net/css?family=Offside";
 			$fontfamilystring = "'Offside', sans-serif";
 			break;
 
 		case "plex_mono":
-			$importurl = "https://fonts.googleapis.com/css?family=IBM+Plex+Mono";
+			$importurl = "https://fonts.bunny.net/css?family=IBM+Plex+Mono";
 			$fontfamilystring = "'IBM Plex Mono', monospace";
 			break;
 
 		case "plex_sans":
-			$importurl = "https://fonts.googleapis.com/css?family=IBM+Plex+Sans";
+			$importurl = "https://fonts.bunny.net/css?family=IBM+Plex+Sans";
 			$fontfamilystring = "'IBM Plex Sans', sans-serif";
 			break;
 
 		case "plex_serif":
-			$importurl = "https://fonts.googleapis.com/css?family=IBM+Plex+Serif";
+			$importurl = "https://fonts.bunny.net/css?family=IBM+Plex+Serif";
 			$fontfamilystring = "'IBM Plex Serif', serif";
 			break;
 
 		case "roboto_condensed":
-			$importurl = "https://fonts.googleapis.com/css?family=Roboto+Condensed";
+			$importurl = "https://fonts.bunny.net/css?family=Roboto+Condensed";
 			$fontfamilystring = "'Roboto Condensed', serif";
 			break;
 
 		case "roboto_slab":
-			$importurl = "https://fonts.googleapis.com/css?family=Roboto+Slab:300";
+			$importurl = "https://fonts.bunny.net/css?family=Roboto+Slab:300";
 			$fontfamilystring = "'Roboto Slab', serif";
 			break;
 
 		case "space_mono":
-			$importurl = "https://fonts.googleapis.com/css?family=Space+Mono";	
+			$importurl = "https://fonts.bunny.net/css?family=Space+Mono";	
 			$fontfamilystring = "'Space Mono', monospace";
 			break;
 
 		case "turret_road":
-			$importurl = "https://fonts.googleapis.com/css?family=Turret+Road:500";
+			$importurl = "https://fonts.bunny.net/css?family=Turret+Road:500";
 			$fontfamilystring = "'Turret Road', sans-serif";
 			break;
 
 		case "ubuntu":
-			$importurl = "https://fonts.googleapis.com/css?family=Ubuntu";
+			$importurl = "https://fonts.bunny.net/css?family=Ubuntu";
 			$fontfamilystring = "'Ubuntu', sans-serif";
 			break;
 
 		case "work_sans":
-			$importurl = "https://fonts.googleapis.com/css?family=Work+Sans";
+			$importurl = "https://fonts.bunny.net/css?family=Work+Sans";
 			$fontfamilystring = "'Work Sans', sans-serif";
 			break;
 
 		case "newsreader":
-			$importurl = "https://fonts.googleapis.com/css?family=Newsreader:wght@500";
+			$importurl = "https://fonts.bunny.net/css?family=Newsreader:wght@500";
 			$fontfamilystring = "'Newsreader', serif";
 			break;
 
 		case "lora":
-			$importurl = "https://fonts.googleapis.com/css?family=Lora";
+			$importurl = "https://fonts.bunny.net/css?family=Lora";
 			$fontfamilystring = "'Lora', serif";
 			break;
 
 		case "bitter":
-			$importurl = "https://fonts.googleapis.com/css?family=Bitter";
+			$importurl = "https://fonts.bunny.net/css?family=Bitter";
 			$fontfamilystring = "'Bitter', serif";
 			break;
 
 		case "source_serif_pro":
-			$importurl = "https://fonts.googleapis.com/css?family=Source+Serif+Pro";
+			$importurl = "https://fonts.bunny.net/css?family=Source+Serif+Pro";
 			$fontfamilystring = "'Source Serif Pro', serif";
 			break;
 
 		default:
-			$importurl = "https://fonts.googleapis.com/css?family=Homemade+Apple";
+			$importurl = "https://fonts.bunny.net/css?family=Homemade+Apple";
 			$fontfamilystring = "'Homemade Apple', cursive";
 			break;
 	}
